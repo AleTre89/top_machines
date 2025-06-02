97eebffab717ef13263db696c06c9da72ba2252c
AleTre89 <alex.tregnago@gmail.com>
2025-06-02 09:58:18 +0200

    Update Principale.py

--- Principale.py
+++ Principale.py
@@ -1,3 +1,8 @@
+"""
+TkInter interface with "anagrafica" and "Manutenzione" buttons that opens respectively
+the objects Anagrafica() and Manutenzioni and allow the access to their functions
+"""
+
 import tkinter
 from tkinter import *
 from UIAnagrafica import Anagrafica
@@ -13,7 +18,7 @@ def manutenzioni_start():
 master_window = Tk()
 master_window.title("Gestione attrezzature")
 
-#Bottoni e posizione
+#Buttons and position
 anagrafica_button = Button(width=15, text= "Anagrafica", highlightthickness=0, command=anagrafica_start)
 manutenzione_button = Button(width=15, text= "Manutenzione", highlightthickness=0,command=manutenzioni_start)
 anagrafica_button.grid(row=0,column=0, padx=20,pady=20)
